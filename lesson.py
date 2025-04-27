7b83ed838da9fa476fdda35c88fb87fd8fc68080
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-04-27 14:55:17 +0900

    :zap: クロージャー

--- lesson.py
+++ lesson.py
@@ -1,5 +1,13 @@
-names = ['Taro', 'Jiro', 'Saburo']
-ages = ['16', '24', '25']
+def circle_area(pi):
+    def cicle_calc(range):
+        return pi * range * range
+    return cicle_calc
 
-for name, age in zip(names, ages):
-    print(name, age)
+fn1 = circle_area(3.14)
+fn2 = circle_area(3.141592)
+
+result1 = fn1(10)
+result2 = fn2(10)
+
+print(result1)
+print(result2)
