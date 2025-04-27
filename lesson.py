8c53f745632eb3127b8f5a8fba9f0376fe4c089d
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-04-27 13:55:47 +0900

    :zap: while文とinput関数

--- lesson.py
+++ lesson.py
@@ -1,15 +1,6 @@
-import math
-name = 'aaaaaaaaaaaaa'
-result = math.sqrt(25)
-print(help(math))
-x = 0
-while x < 5:
-    if x == 3:
-        x += 1
 
-        continue
-    print(x)
-
-    x += 1
-else:
-    print(x)
+while True:
+    word = input('Enter:')
+    if word == 'ok':
+        break
+    print('next')
