01bebc8c6beb09ee40494be58e62b057c8c4c541
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-04-27 15:32:45 +0900

    :zap: デコレータを使用すると関数に処理を追加できる

--- lesson.py
+++ lesson.py
@@ -10,7 +10,14 @@ def decolator_func(fn):
 def add_fn(a, b):
     return a + b
 
+@decolator_func
+def minus_fn(a, b):
+    return a - b
+
 
 result = add_fn(10, 7)
 print(result)
+
+result2 = minus_fn(20, 8)
+print(result2)
 
