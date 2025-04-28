b9556a765fabc32be965a0245916c4140244e8c8
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-04-28 12:56:04 +0900

    :zap: ファイルへの書き込み

--- lesson.py
+++ lesson.py
@@ -1,16 +1,2 @@
-class Person(object):
-    def talk(self):
-        print('person talk')
-
-class Car(object):
-    def drive(self):
-        print('car drive')
-
-class Person_Car_Robot(Person, Car):
-    def fly(self):
-        print('I can fly')
-
-person_car_robot = Person_Car_Robot()
-person_car_robot.talk()
-person_car_robot.drive()
-person_car_robot.fly()
+with open('test.txt', 'w') as f:
+    f.write('Test')
