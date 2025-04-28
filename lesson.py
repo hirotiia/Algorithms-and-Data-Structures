a02f1e0b386413d01f00402e1df7a89891d8ea13
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-04-28 10:29:58 +0900

    :zap: コマンドライン引数：コマンドラインからの実行時に引数を渡してシステム内で使用することができる

--- lesson.py
+++ lesson.py
@@ -1,9 +1,2 @@
-l = ['Mon', 'tue', 'Wed', 'thu', 'Fri', 'sat', 'Sun']
-
-def change_words(words, fn):
-    for word in words:
-        print(fn(word))
-
-
-change_words(l, lambda word: word.capitalize())
-change_words(l, lambda word: word.lower())
+import sys
+print(sys.argv)
