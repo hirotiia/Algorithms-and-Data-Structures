d0aeeeeff7e516f3e9aa5e42530fc194f64d0fd8
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-04-30 18:06:30 +0900

    :zap: エラー発生場所を特定

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import roboter.controller.conversation
 
+class MainError(Exception):
+    pass
+
 def main():
     roboter.controller.conversation.talk_about_restaurant()
+    raise MainError
 
 if __name__ == '__main__':
     main()
